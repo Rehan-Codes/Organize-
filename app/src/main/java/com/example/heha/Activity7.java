1824578af09f2965496b95a7fa5a57810487bb93
RehanComputerGuy <70353481+RehanComputerGuy@users.noreply.github.com>
2020-09-09 18:19:32 -0400

    Final Button to go to main menu

--- app/src/main/java/com/example/heha/Activity7.java
+++ app/src/main/java/com/example/heha/Activity7.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.view.DragEvent;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.content.ClipData;
 import android.os.Bundle;
@@ -16,7 +17,8 @@ import android.view.View;
 public class Activity7 extends AppCompatActivity {
 
     LinearLayout target1, target2, target3, target4;
-    Button test1, test2, test3, test4, btn1, btn2, btn3, btn4, submit, backToMenu, resetChange;
+    Button test1, test2, test3, test4, submit, backToMenu, resetChange;
+    ImageView btn1, btn2, btn3, btn4;
     int i = 0;
     int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
     @Override
@@ -36,10 +38,10 @@ public class Activity7 extends AppCompatActivity {
         test3 = (Button) findViewById(R.id.test3);
         test4 = (Button) findViewById(R.id.test4);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
+        btn1 = (ImageView) findViewById(R.id.btn1);
+        btn2 = (ImageView) findViewById(R.id.btn2);
+        btn3 = (ImageView) findViewById(R.id.btn3);
+        btn4 = (ImageView) findViewById(R.id.btn4);
         submit = (Button) findViewById(R.id.submit);
 
         target1.setOnDragListener(dragListenre);
