c3c9518c20b6dd3624077c10a23e0daf96c8cd45
Jazib16671 <871963@pdsb.net>
2020-09-09 21:19:05 -0400

    Test

--- app/src/main/java/com/example/heha/MainActivity.java
+++ app/src/main/java/com/example/heha/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
