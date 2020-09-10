a41d04955ef735446144a878cd262b31516bec84
Parthiv18 <56895655+Parthiv18@users.noreply.github.com>
2020-09-10 17:53:32 -0400

    Commented it out test

--- app/src/main/java/com/example/heha/MainActivity.java
+++ app/src/main/java/com/example/heha/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.heha;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,17 +14,34 @@ import com.example.heha.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button, btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 openActivity2();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Guide");
+                alertDialog.setMessage("- Select one of the five categories\n- Select one of the levels\n- Sort the pictures according to the given requirements!");
+                alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Close",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
             }
         });
     }
