12ec7beec92211e454106c63f44b2f97b1faed55
Jazib16671 <871963@pdsb.net>
2020-10-13 16:51:15 -0400

    Jazib's push

--- app/src/main/java/com/example/heha/Activity33.java
+++ app/src/main/java/com/example/heha/Activity33.java
@@ -110,7 +110,7 @@ public class Activity33 extends AppCompatActivity {
                     case DragEvent.ACTION_DROP:
 
                         if (v.getId() == R.id.target1) {
-                            if (view.getId() == R.id.btn1) {
+                            if (view.getId() == R.id.btn2) {
                                 num1 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -123,7 +123,7 @@ public class Activity33 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target2) {
-                            if (view.getId() == R.id.btn3) {
+                            if (view.getId() == R.id.btn4) {
                                 num2 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -136,7 +136,7 @@ public class Activity33 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target3) {
-                            if (view.getId() == R.id.btn2) {
+                            if (view.getId() == R.id.btn3) {
                                 num3 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -149,7 +149,7 @@ public class Activity33 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target4) {
-                            if (view.getId() == R.id.btn4) {
+                            if (view.getId() == R.id.btn1) {
                                 num4 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -177,7 +177,7 @@ public class Activity33 extends AppCompatActivity {
             if (num1 == 1 && num2 == 1 && num3 == 1 && num4 == 1) {
                 AlertDialog alertDialog = new AlertDialog.Builder(Activity33.this).create();
                 alertDialog.setTitle("Level Passed \u2713");
-                alertDialog.setMessage("Ferrari 458 - $250-500k CAD\nRottweiler Dog - $2-5k CAD\nROLEX DEEPSEA Watch - 15-20k CAD\nSuper Yacht - 250-500k CAD");
+                alertDialog.setMessage("Jellyfish - 8km/h\nBeluga Whale - 28km/h\nShark - 50km/h\nDolphin - 60km/h");
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Next Level",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
--- app/src/main/java/com/example/heha/Activity34.java
+++ app/src/main/java/com/example/heha/Activity34.java
@@ -110,7 +110,7 @@ public class Activity34 extends AppCompatActivity {
                     case DragEvent.ACTION_DROP:
 
                         if (v.getId() == R.id.target1) {
-                            if (view.getId() == R.id.btn1) {
+                            if (view.getId() == R.id.btn3) {
                                 num1 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -123,7 +123,7 @@ public class Activity34 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target2) {
-                            if (view.getId() == R.id.btn3) {
+                            if (view.getId() == R.id.btn1) {
                                 num2 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -177,7 +177,7 @@ public class Activity34 extends AppCompatActivity {
             if (num1 == 1 && num2 == 1 && num3 == 1 && num4 == 1) {
                 AlertDialog alertDialog = new AlertDialog.Builder(Activity34.this).create();
                 alertDialog.setTitle("Level Passed \u2713");
-                alertDialog.setMessage("Ferrari 458 - $250-500k CAD\nRottweiler Dog - $2-5k CAD\nROLEX DEEPSEA Watch - 15-20k CAD\nSuper Yacht - 250-500k CAD");
+                alertDialog.setMessage("Pigeon - 60km/h\nHummingbird - 80km/h\nCrow - 100km/h\nBald Eagle -160km/h");
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Next Level",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
