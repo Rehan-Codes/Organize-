0f55845ae05abc89a2570ff8cdc95476cd66665f
Jazib16671 <871963@pdsb.net>
2020-10-12 20:07:02 -0400

    Jazib's push

--- app/src/main/java/com/example/heha/Activity28.java
+++ app/src/main/java/com/example/heha/Activity28.java
@@ -123,7 +123,7 @@ public class Activity28 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target2) {
-                            if (view.getId() == R.id.btn3) {
+                            if (view.getId() == R.id.btn4) {
                                 num2 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -149,7 +149,7 @@ public class Activity28 extends AppCompatActivity {
                             i++;
                             checki(i);
                         } else if (v.getId() == R.id.target4) {
-                            if (view.getId() == R.id.btn4) {
+                            if (view.getId() == R.id.btn3) {
                                 num4 = 1;
                             }
                             view.setOnLongClickListener(null);
@@ -177,7 +177,7 @@ public class Activity28 extends AppCompatActivity {
             if (num1 == 1 && num2 == 1 && num3 == 1 && num4 == 1) {
                 AlertDialog alertDialog = new AlertDialog.Builder(Activity28.this).create();
                 alertDialog.setTitle("Level Passed \u2713");
-                alertDialog.setMessage("Ferrari 458 - $250-500k CAD\nRottweiler Dog - $2-5k CAD\nROLEX DEEPSEA Watch - 15-20k CAD\nSuper Yacht - 250-500k CAD");
+                alertDialog.setMessage("Turtle - 0.5km/h\nHumans(average) - 20km/h\nRabbit(average) - 48km/h\nCheetah - 80-130km/h");
                 alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Next Level",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
