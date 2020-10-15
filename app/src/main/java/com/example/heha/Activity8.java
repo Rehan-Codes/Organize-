dc1e819df9759ff49d8219b3de04dc6c6ac07c17
RehanComputerGuy <70353481+RehanComputerGuy@users.noreply.github.com>
2020-10-15 02:45:38 -0400

    Merge remote-tracking branch 'github/master' into master
    # Conflicts:
    #	app/src/main/res/layout/activity_7.xml

--- app/src/main/java/com/example/heha/Activity8.java
+++ app/src/main/java/com/example/heha/Activity8.java
@@ -186,7 +186,7 @@ public class Activity8 extends AppCompatActivity {
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
-                                Intent intent2 = new Intent(Activity8.this, Activity8.class);
+                                Intent intent2 = new Intent(Activity8.this, Activity9.class);
                                 startActivity(intent2);
                             }
                         });
